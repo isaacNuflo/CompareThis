73ce56a7f25330462d00cd7a1c269ac4b0bd3a67
isaacNuflo <isaac.nuflo21@gmail.com>
2019-08-04 20:52:49 -0500

    init

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+import argparse
+import json
+import re
+import ast
+
+API_KEY = 'MY_API_KEY'
+with open('libros.json') as json_file:
+    libros = json.load(json_file)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-v", "--versiculo", help="Versiculo a consultar")
+args = parser.parse_args()
+
+if args.versiculo:
+    libro = re.search(r'[1-2]?[\s]?[a-zA-Z]+', args.versiculo).group(0)
+    cita = re.search(r'[1-9]\d{0,2}\:[1-9]\d{0,2}', args.versiculo).group(0)
+    if libro and cita:
+        libro_ingles = libros[libro]
+        cita = cita.replace(":", ".")
+
+        request = 'https://api.biblia.com/v1/bible/content/RVR60.txt.json?passage={}&key={}'.format(
+            libro_ingles + cita, API_KEY)
+
+        response = requests.get(request)
+
+        texto = json.loads(response.content.decode('utf-8'))["text"]
+
+        print(texto)
+    else:
+        print('Versiculo mal escrito')
+else:
+    print('Ingrese un versiculo a consultar')
